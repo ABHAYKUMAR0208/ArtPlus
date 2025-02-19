500d51fcd39da81127bbbd58ac7f720705fb0105
Abhay Kumar <120698096+ABHAYKUMAR0208@users.noreply.github.com>
2025-02-20 00:46:43 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -61,7 +61,8 @@ function App() {
   const location = useLocation();
 
   useEffect(() => {
-    dispatch(checkAuth());
+    const token = JSON.parse(sessionStorage.getItem('token'))
+    dispatch(checkAuth(token));
   }, [dispatch]);
 
   if (isLoading) {
