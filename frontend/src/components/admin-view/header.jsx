81870e0b2a61fae5d369c8dcad3c51d7e9501e9a
Abhay Kumar <120698096+ABHAYKUMAR0208@users.noreply.github.com>
2025-02-24 17:57:31 +0530

    Update header.jsx

--- frontend/src/components/admin-view/header.jsx
+++ frontend/src/components/admin-view/header.jsx
@@ -4,6 +4,7 @@ import { Button } from "../ui/button";
 import { LogOut } from "lucide-react";
 import { useDispatch } from "react-redux";
 import { logoutUser } from "@/store/auth-slice";
+import { useNavigate } from "react-router-dom";
 
 function AdminHeader({ setOpen }) {
 
